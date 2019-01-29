20401b38971886037422eeda365819a331e38623
Robert Lancaster <rlancaste@gmail.com>
2019-01-28 20:14:54 -0500

    Fixing netpbm

--- libs/netpbmForKStars/netpbmForKStars.py
+++ libs/netpbmForKStars/netpbmForKStars.py
@@ -26,15 +26,14 @@ from Package.MakeFilePackageBase import *
 class Package(MakeFilePackageBase):
     def __init__(self, **args):
         MakeFilePackageBase.__init__(self)
-        self.subinfo.options.useShadowBuild = False
+        self.subinfo.options.useShadowBuild = True
         
     def configure(self):
         self.enterSourceDir()
-        prefixdir = self.imageDir()
         craftLibDir = os.path.join(CraftCore.standardDirs.craftRoot(), 'lib')
         craftIncludeDir = os.path.join(CraftCore.standardDirs.craftRoot(), 'include')
         builddebug = "yes" if self.buildType() == "Debug" else "no"
-        utils.copyFile(os.path.join(self.sourceDir(), "config.mk.in"), os.path.join(self.sourceDir(), "bin", "config.mk"), linkOnly=False)
+        utils.copyFile(os.path.join(self.sourceDir(), "config.mk.in"), os.path.join(self.sourceDir(), "config.mk"), linkOnly=False)
         f1name = os.path.join(self.sourceDir(), "config.mk.in")
         f2name = os.path.join(self.sourceDir(), "config.mk")
         f1 = open(f1name, 'r')
@@ -70,23 +69,30 @@ class Package(MakeFilePackageBase):
         f2.close()
         return True
         
-    def install(self):
-        if not MakeFilePackageBase.install(self):
-            return False
+    def make(self):
         self.enterSourceDir()
+        utils.system("make")
+        self.cleanImage()
+        # Note that it has to be in a child folder, otherwise netpbm won't build it.
+        buildDir=os.path.join(self.buildDir(), 'build')
+        utils.system("make package pkgdir=" + buildDir)
+        return True
+        
+    def install(self):
+        utils.system("make package pkgdir=" + self.imageDir())
         # Deleting the netpbm files which have licensing restrictions on distribution or are unknown status
-        utils.deleteFile(os.path.join(self.sourceDir(),'hpcdtoppm'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pcdindex'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'ppmtogif'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pamchannel'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pamtopnm'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pbmto4425'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pbmtoln03'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pbmtolps'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pbmtopk'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'pktopbm'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'ppmtopjxl'))
-        utils.deleteFile(os.path.join(self.sourceDir(),'spottopgm'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'hpcdtoppm'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pcdindex'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'ppmtogif'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pamchannel'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pamtopnm'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pbmto4425'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pbmtoln03'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pbmtolps'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pbmtopk'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'pktopbm'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'ppmtopjxl'))
+        utils.deleteFile(os.path.join(self.imageDir(), 'bin', 'spottopgm'))
         
         #This is for the one in a package for KStars
         netpbmDir = os.path.join(CraftCore.standardDirs.craftRoot(), 'netpbm')
@@ -98,9 +104,9 @@ class Package(MakeFilePackageBase):
         libDir = os.path.join(netpbmDir,  'lib')
         
         utils.system("mkdir -p " + netpbmDir)
+        
         utils.system("mkdir -p " + binaryDir)
         utils.system("mkdir -p " + libDir)
-        utils.system("make package pkgdir=" + self.imageDir())
         utils.system("cp " + self.imageDir() + "/bin/* " + binaryDir)
         utils.system("cp -r " + self.imageDir() + "/include/* " + includeDir)
         utils.system("cp " + self.imageDir() + "/lib/* " + libDir)
