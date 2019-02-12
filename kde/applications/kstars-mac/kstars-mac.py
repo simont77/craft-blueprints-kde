d95a59df7acb20441395d6144fb2234cc22abaa2
Robert Lancaster <rlancaste@gmail.com>
2019-02-11 23:21:56 -0500

    Deleting tabs

--- kde/applications/kstars-mac/kstars-mac.py
+++ kde/applications/kstars-mac/kstars-mac.py
@@ -199,13 +199,13 @@ class Package(CMakePackageBase):
         #	Editing the info.plist file
         pListFile = KSTARS_APP + "/Contents/info.plist"
 
-		utils.system("plutil -insert NSPrincipalClass -string NSApplication " + pListFile)
-		utils.system("plutil -insert NSHighResolutionCapable -string True " + pListFile)
-		utils.system("plutil -insert NSRequiresAquaSystemAppearance -string NO " + pListFile)
-		utils.system("plutil -replace CFBundleName -string KStars " + pListFile)
-		utils.system("plutil -replace CFBundleVersion -string ${KSTARS_VERSION} " + pListFile)
-		utils.system("plutil -replace CFBundleLongVersionString -string ${KSTARS_VERSION} " + pListFile)
-		utils.system("plutil -replace CFBundleShortVersionString -string ${KSTARS_VERSION} " + pListFile)
-		utils.system("plutil -replace NSHumanReadableCopyright -string '© 2001 - 2018, The KStars Team, Freely Released under GNU GPL V2'"  + pListFile)
+        utils.system("plutil -insert NSPrincipalClass -string NSApplication " + pListFile)
+        utils.system("plutil -insert NSHighResolutionCapable -string True " + pListFile)
+        utils.system("plutil -insert NSRequiresAquaSystemAppearance -string NO " + pListFile)
+        utils.system("plutil -replace CFBundleName -string KStars " + pListFile)
+        utils.system("plutil -replace CFBundleVersion -string ${KSTARS_VERSION} " + pListFile)
+        utils.system("plutil -replace CFBundleLongVersionString -string ${KSTARS_VERSION} " + pListFile)
+        utils.system("plutil -replace CFBundleShortVersionString -string ${KSTARS_VERSION} " + pListFile)
+        utils.system("plutil -replace NSHumanReadableCopyright -string '© 2001 - 2018, The KStars Team, Freely Released under GNU GPL V2'"  + pListFile)
 
         return True
