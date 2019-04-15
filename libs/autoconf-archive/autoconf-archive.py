6f1b63bfc8bf8c3e9027fcd8367d0951d3cba696
Robert Lancaster <rlancaste@gmail.com>
2019-04-14 22:18:06 -0400

    adding dbus recipe since the craft dbus does not work correctly

---
+++ libs/autoconf-archive/autoconf-archive.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["2019.01.06"]:
+            self.targets[ver] = f"https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-{ver}.tar.xz"
+            self.archiveNames[ver] = f"autoconf-archive-{ver}.tar.xz"
+            self.targetInstSrc[ver] = "autoconf-archive-%s" % ver
+        self.description = "Collection of over 500 reusable autoconf macros"
+        self.webpage = "https://savannah.gnu.org/projects/autoconf-archive/"
+        self.defaultTarget = "2019.01.06"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        #self.runtimeDependencies["autoconf"] = None
+
+from Package.AutoToolsPackageBase import *
+
+class Package(AutoToolsPackageBase):
+    def __init__( self, **args ):
+        AutoToolsPackageBase.__init__( self )
+        prefix = self.shell.toNativePath(CraftCore.standardDirs.craftRoot())
+        self.subinfo.options.configure.bootstrap = True
+        self.subinfo.options.useShadowBuild = True
+        self.subinfo.options.configure.args += " --prefix=" + prefix
