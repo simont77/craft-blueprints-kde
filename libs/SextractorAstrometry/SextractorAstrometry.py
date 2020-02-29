c3cdabfca242798dcbcda44c332854ee67cd825d
Robert Lancaster <rlancaste@gmail.com>
2020-02-29 00:54:26 -0500

    Adding the SextractorAstrometry tester for experimental work in windows

---
+++ libs/SextractorAstrometry/SextractorAstrometry.py
@@ -0,0 +1,32 @@
+import glob
+from xml.etree import ElementTree as et
+
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = 'Sextractor and Astrometry Library Tester Program'
+        self.svnTargets['Latest'] = "https://github.com/rlancaste/astrometry-sextractor-tester.git"
+        self.targetInstSrc['Latest'] = ""
+
+        self.defaultTarget = 'Latest'
+    
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["libs/gsl"] = "default"
+        self.runtimeDependencies["libs/cfitsio"] = "default"
+
+
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+        root = CraftCore.standardDirs.craftRoot()
+        craftLibDir = os.path.join(root,  'lib')
+        self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DBUILD_LIBS=1 -DCMAKE_INSTALL_RPATH=" + craftLibDir
